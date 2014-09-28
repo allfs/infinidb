ee74474ee167d7c9c19195ee5705168d47019332
root <root@root>
2014-09-27 20:15:31 -0400

    bump supported keys

--- dbcon/mysql/ha_calpont.h
+++ dbcon/mysql/ha_calpont.h
@@ -137,7 +137,7 @@ public:
     There is no need to implement ..._key_... methods if your engine doesn't
     support indexes.
    */
-  uint32_t max_supported_key_parts()     const { return 1; }
+  uint32_t max_supported_key_parts()     const { return 16; }
 
   /** @brief
     unireg.cc will call this to make sure that the storage engine can handle
